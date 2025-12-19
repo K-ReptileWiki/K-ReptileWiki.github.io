3bd49589b638469488c3408d8028b6b418ad0bc8
K-ReptileWiki <limsemin17@gmail.com>
2025-12-19 18:00:04 +0900

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,19 @@
+// firebase.js
+import { initializeApp, getApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-storage.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDfrvgcAed9VvS5MFXVZFIxch8aCAfMp1w",
+  authDomain: "k-reptilewiki-1f09f.firebaseapp.com",
+  projectId: "k-reptilewiki-1f09f"
+};
+
+// 앱 초기화 (중복 방지)
+const app = initializeApp(firebaseConfig);
+
+// 모듈 export
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
