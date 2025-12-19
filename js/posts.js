9911152dd3ac5f1ac4e0b645768acdb81a1a2632
K-ReptileWiki <limsemin17@gmail.com>
2025-12-19 18:01:08 +0900

    Update posts.js

--- js/posts.js
+++ js/posts.js
@@ -1,48 +1,27 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+import { db, auth, storage } from "./firebase.js";
 import {
-  getFirestore, collection, addDoc, onSnapshot, serverTimestamp
+  collection, addDoc, onSnapshot, serverTimestamp
 } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
 import {
-  getAuth, signInAnonymously, onAuthStateChanged
+  signInAnonymously, onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
-import {
-  getStorage, ref, uploadBytes, getDownloadURL
-} from "https://www.gstatic.com/firebasejs/12.7.0/firebase-storage.js";
+import { ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-storage.js";
 
-/* Firebase 초기화 */
-const app = initializeApp({
-  apiKey: "AIzaSyDfrvgcAed9VvS5MFXVZFIxch8aCAfMp1w",
-  authDomain: "k-reptilewiki-1f09f.firebaseapp.com",
-  projectId: "k-reptilewiki-1f09f"
-});
-const db = getFirestore(app);
-const auth = getAuth(app);
-const storage = getStorage(app);
-
-/* Quill 에디터 초기화 */
+// Quill 에디터 초기화
 const quill = new Quill('#editor', {
   theme: 'snow',
   modules: {
     toolbar: [
-      ['bold', 'italic', 'underline'], // 굵기, 기울임, 밑줄
-      [{ 'header': [1, 2, 3, 4, 5, 6, false] }], // h1~h6 크기
-      [{ 'color': [
-        '#000000', // 검정
-        '#FF0000', // 빨강
-        '#0000FF', // 파랑
-        '#008000', // 초록
-        '#FFA500', // 주황
-        '#800080', // 보라
-        '#A52A2A', // 갈색
-        '#FFD700'  // 금색
-      ] }],
-      [{ 'background': [] }], // 배경색
-      ['link'] // 링크 삽입
+      ['bold', 'italic', 'underline'],
+      [{ 'header': [1,2,3,4,5,6,false] }],
+      [{ 'color': ['#000000','#FF0000','#0000FF','#008000','#FFA500','#800080','#A52A2A','#FFD700'] }],
+      [{ 'background': [] }],
+      ['link']
     ]
   }
 });
 
-/* 로그인 */
+// 로그인
 signInAnonymously(auth);
 
 let currentUser = null;
@@ -55,7 +34,6 @@ onAuthStateChanged(auth, async (user) => {
   initPosts();
 });
 
-/* 글 작성 및 목록 */
 function initPosts() {
   const postsRef = collection(db, "wiki_posts");
 
@@ -68,7 +46,6 @@ function initPosts() {
       const div = document.createElement("div");
       div.className = "post-card";
 
-      // 이미지 표시
       let imgHtml = "";
       if (p.images && Array.isArray(p.images)) {
         imgHtml = p.images.map(url => `<img src="${url}" style="max-width:200px;margin:5px;">`).join("");
@@ -93,7 +70,6 @@ function initPosts() {
     if (!title || !content) return alert("제목과 내용을 입력하세요");
     if (files.length > 3) return alert("사진은 최대 3장까지 첨부 가능합니다");
 
-    // 이미지 업로드
     const imageUrls = [];
     for (let i = 0; i < files.length; i++) {
       const file = files[i];
@@ -103,7 +79,6 @@ function initPosts() {
       imageUrls.push(url);
     }
 
-    // Firestore에 글 저장
     await addDoc(postsRef, {
       uid: currentUser.uid,
       author: userData.nickname,
