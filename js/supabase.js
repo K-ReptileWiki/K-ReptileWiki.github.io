39e52303256cfa6ddcce34d1d5d5e4e5390845c2
K-ReptileWiki <limsemin17@gmail.com>
2025-12-20 20:05:39 +0900

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -3,7 +3,7 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 // 프로젝트 URL과 anon 키
 const supabaseUrl = "https://cpaikpjzlzzujwfgnanb.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."; // anon(public) 키
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNwYWlrcGp6bHp6dWp3ZmduYW5iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYxNDEwMzIsImV4cCI6MjA4MTcxNzAzMn0.u5diz_-p8Hh1FtkVO1CsDSUbz9fbSN2zXAIIP2637sc"; // anon(public) 키
 
 // export 한 번만!
 export const supabase = createClient(supabaseUrl, supabaseKey);
