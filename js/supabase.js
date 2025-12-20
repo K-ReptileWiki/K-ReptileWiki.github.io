752a06d985fdfd8d030fe0a72e4ec860c2ca9b27
K-ReptileWiki <limsemin17@gmail.com>
2025-12-20 19:52:02 +0900

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 const supabase = createClient("https://cpaikpjzlzzujwfgnanb.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNwYWlrcGp6bHp6dWp3ZmduYW5iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYxNDEwMzIsImV4cCI6MjA4MTcxNzAzMn0.u5diz_-p8Hh1FtkVO1CsDSUbz9fbSN2zXAIIP2637sc");
-
+export const supabase = createClient(supabaseUrl, supabaseKey);
 let currentUser = null;
 let userData = { nickname: "익명", role: "user", lastPostAt: 0 };
 
